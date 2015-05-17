2719c623907da7dbd59fe88096aa7afd81160e3b
Dan Gopstein <dan@gopstein.com>
2015-05-17 14:49:01 -0400

    Parse shifted values

--- clojure/keylog-reader/keylog_reader.clj
+++ clojure/keylog-reader/keylog_reader.clj
@@ -10,6 +10,15 @@
 (def re-char #".")
 (def re-token (re-pattern (str "(?s)" re-meta "|" re-char)))
 
+(def lowercase "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./")
+(def uppercase "~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?")
+
+(def downcase-map (zipmap (re-seq #"." uppercase) (re-seq #"." lowercase)))
+
+(defn parse-shift [token]
+  (let [downcase (downcase-map token)]
+    (if downcase ["<SHIFT>" downcase] [token])))
+
 (defn -main [& args]
  (let [[opts argv banner] (cli args
                            ["-h" "--help" "Print this help"
@@ -21,8 +30,8 @@
   (let [filename (first args)
         file  (slurp filename)
         tokens (re-seq re-token file)
-        counts (frequencies tokens)
+        parsed (mapcat parse-shift tokens)
+        counts (frequencies parsed)
         sorted (sort-by val > counts)]
-    (println sorted)
-    )))
+    (println sorted))))
 
