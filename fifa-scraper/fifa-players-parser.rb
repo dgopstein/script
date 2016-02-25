ec8ea03bb9a473db24f951bd99d1048bd392dfbe
Dan Gopstein <dan@gopstein.com>
2016-02-24 19:38:22 -0500

    Fifa keepers

--- fifa-scraper/fifa-players-parser.rb
+++ fifa-scraper/fifa-players-parser.rb
@@ -2,10 +2,11 @@
 
 require 'csv'
 
-filepat = 'players_html/players_%03d.html'
+#filepat = 'players_html/players_%03d.html'
+filepat = 'players_html/keepers_%02d.html'
 
-CSV.open("players.csv", "w") do |csv|
-  (1..313).each do |page_num|
+CSV.open("keepers.csv", "w") do |csv|
+  (1..39).each do |page_num|
     html = File.open(filepat % page_num).read
     names = html.scan(%r{class="name">\s*(.*?)<br />.*?<span>(.*?)</span>.*?position">(.*?)<.*?rating.*?>.*?>\s*(\d\d)}m)
     names.each{ |name|
--- fifa-scraper/fifa-players.sh
+++ fifa-scraper/fifa-players.sh
@@ -1,4 +1,6 @@
-for i in `seq -w 1 320`; do
-  curl "http://www.futhead.com/16/career-mode/players/?page=$i" > players_$i.html
+#for i in `seq -w 1 320`; do
+  #curl "http://www.futhead.com/16/career-mode/players/?page=$i" > players_$i.html
+for i in `seq -w 1 40`; do
+  curl "http://www.futhead.com/16/career-mode/players/?group=gk&page=$i" > keepers_$i.html
   sleep 0.5
 done
